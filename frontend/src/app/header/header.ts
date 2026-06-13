21ebf5831cb9acc2b54bd7c5a1ed97d511e9cc19
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-06-13 23:33:36 +0200

    [Frontend] Added logout button into header component

--- frontend/src/app/header/header.ts
+++ frontend/src/app/header/header.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthorizationService } from '../services/auth/authorization.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  authService = inject(AuthorizationService);
+
+  logout() : void {
+    this.authService.logout();
+  }
+
+}
