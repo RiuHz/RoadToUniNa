55c0e653e26d2607fba3db8030591e2397d22092
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-06-13 23:30:02 +0200

    [Frontend] Added updated package.json with modules, app routes / config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { authorizationInterceptor } from './interceptors/auth/authorization.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideToastr({
+      progressBar: true,
+      newestOnTop: true
+    }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authorizationInterceptor])
+    ),
     provideRouter(routes)
   ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { MatchHistory } from './match-history/match-history';
+import { Match } from './match/match';
+import { authorizationGuard } from './guards/auth/authorization.guard';
+import { MatchInfo } from './match-info/match-info';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'RoadToUniNa',
+        component: Homepage
+    },
+    {
+        path: 'leaderboard',
+        title: 'Classifiche | RoadToUniNa',
+        component: Leaderboard
+    },
+    {
+        path: 'match-history',
+        title: 'Storico Partite | RoadToUniNa',
+        component: MatchHistory
+    },
+    {
+        path: 'match',
+        title: 'Partita | RoadToUniNa',
+        component: Match,
+        canActivate: [authorizationGuard]
+    },
+    {
+        path: 'match/:id',
+        component: MatchInfo
+    },
+    {
+        path: 'login',
+        title: 'Login | RoadToUniNa',
+        component: Login
+    },
+    {
+        path: 'signup',
+        title: 'Sign up | RoadToUniNa',
+        component: Signup
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
